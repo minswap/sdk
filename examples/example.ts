4fce42b48e3bda250d71b8f36f108ed694b52567
Ha Quang Minh <haquangminhbnvn@gmail.com>
2024-09-18 16:32:22 +0700

    grammar

--- examples/example.ts
+++ examples/example.ts
@@ -866,9 +866,9 @@ async function _swapStableExample(
 
   const swapAmount = 1_000n;
 
-  // This pool have 2 assets in config. They are [tDJED, tiUSD].
+  // This pool has 2 assets in its config. They are [tDJED, tiUSD].
   // Index-0 Asset is tDJED. Index-1 Asset is tiUSD.
-  // This order swap 1_000n tDJED to ... tiUSD.
+  // This order swaps 1_000n tDJED to ... tiUSD.
   const amountOut = StableswapCalculation.calculateSwapAmount({
     inIndex: 0,
     outIndex: 1,
@@ -913,8 +913,8 @@ async function _depositStableExample(
 
   const pool = await blockfrostAdapter.getStablePoolByLpAsset(lpAsset);
 
-  // This pool have 2 assets in config. They are [tDJED, tiUSD].
-  // This order deposit 100_000n tDJED and 1_000n tiUSD to pool.
+  // This pool has 2 assets in its config. They are [tDJED, tiUSD].
+  // This order deposits 100_000n tDJED and 1_000n tiUSD into the pool.
   const amountIns = [100_000n, 1_000n];
 
   const lpAmount = StableswapCalculation.calculateDeposit({
@@ -1003,8 +1003,8 @@ async function _withdrawImbalanceStableExample(
 
   const withdrawAmounts = [1234n, 5678n];
 
-  // This pool have 2 assets in config. They are [tDJED, tiUSD].
-  // This order withdraw exactly 1234n tDJED and 5678n tiUSD from pool.
+  // This pool has 2 assets in its config. They are [tDJED, tiUSD].
+  // This order withdraws exactly 1234n tDJED and 5678n tiUSD from the pool.
   const lpAmount = StableswapCalculation.calculateWithdrawImbalance({
     withdrawAmounts: withdrawAmounts,
     totalLiquidity: pool.totalLiquidity,
@@ -1046,8 +1046,8 @@ async function _zapOutStableExample(
 
   const pool = await blockfrostAdapter.getStablePoolByLpAsset(lpAsset);
 
-  // This pool have 2 assets in config. They are [tDJED, tiUSD].
-  // This order withdraw xxx tiUSD by 12345 Lp Asset from pool.
+  // This pool has 2 assets in its config. They are [tDJED, tiUSD].
+  // This order withdraws xxx tiUSD by 12345 Lp Assets from the pool.
   const lpAmount = 12345n;
   const outIndex = 0;
   const amountOut = StableswapCalculation.calculateZapOut({
