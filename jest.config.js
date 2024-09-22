184e00aa279c8504bb76c7d389327404944a5c59
Nhat Khanh <lednhatkhanh@gmail.com>
2024-09-22 11:19:13 +0700

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -144,7 +144,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
