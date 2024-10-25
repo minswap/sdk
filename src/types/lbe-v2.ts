a3b6c16908b4fadc1b7e50e0f3bace3303698546
Ha Quang Minh <haquangminhbnvn@gmail.com>
2024-10-25 10:54:08 +0700

    use custom schema

--- src/types/lbe-v2.ts
+++ src/types/lbe-v2.ts
@@ -1,108 +1,7 @@
-import { Address, Data } from "lucid-cardano";
+import { Address, Constr, Data } from "lucid-cardano";
 
-import { Asset } from "..";
-import {
-  FactoryValidateFactory,
-  FactoryValidateFactoryMinting,
-  ManagerValidateManagerSpending,
-  OrderValidateOrder,
-  SellerValidateSellerSpending,
-  TreasuryValidateTreasurySpending,
-} from "./lbe-v2-plutus";
+import { Asset, LbeV2Constant, NetworkId } from "..";
 import { AddressPlutusData } from "./address.internal";
-
-export namespace LbeV2PlutusSchema {
-  export type FactoryDatum = FactoryValidateFactory["datum"];
-  export type TreasuryDatum =
-    TreasuryValidateTreasurySpending["treasuryInDatum"];
-  export type ManagerDatum = ManagerValidateManagerSpending["managerInDatum"];
-  export type SellerDatum = SellerValidateSellerSpending["sellerInDatum"];
-  export type OrderDatum = OrderValidateOrder["datum"];
-
-  export function toFactoryDatum(datum: FactoryDatum): string {
-    return Data.to(datum, FactoryValidateFactory.datum);
-  }
-  export function fromFactoryDatum(rawDatum: string): FactoryDatum {
-    return Data.from(rawDatum, FactoryValidateFactory.datum);
-  }
-
-  export function toTreasuryDatum(datum: TreasuryDatum): string {
-    return Data.to(datum, TreasuryValidateTreasurySpending.treasuryInDatum);
-  }
-  export function fromTreasuryDatum(rawDatum: string): TreasuryDatum {
-    return Data.from(
-      rawDatum,
-      TreasuryValidateTreasurySpending.treasuryInDatum
-    );
-  }
-
-  export function toManagerDatum(datum: ManagerDatum): string {
-    return Data.to(datum, ManagerValidateManagerSpending.managerInDatum);
-  }
-  export function fromManagerDatum(rawDatum: string): ManagerDatum {
-    return Data.from(rawDatum, ManagerValidateManagerSpending.managerInDatum);
-  }
-
-  export function toSellerDatum(datum: SellerDatum): string {
-    return Data.to(datum, SellerValidateSellerSpending.sellerInDatum);
-  }
-  export function fromSellerDatum(rawDatum: string): SellerDatum {
-    return Data.from(rawDatum, SellerValidateSellerSpending.sellerInDatum);
-  }
-
-  export function toOrderDatum(datum: OrderDatum): string {
-    return Data.to(datum, OrderValidateOrder.datum);
-  }
-  export function fromOrderDatum(rawDatum: string): OrderDatum {
-    return Data.from(rawDatum, OrderValidateOrder.datum);
-  }
-
-  export type FactoryRedeemer = FactoryValidateFactory["redeemer"];
-  export type ManagerRedeemer = ManagerValidateManagerSpending["redeemer"];
-  export type MintRedeemer = FactoryValidateFactoryMinting["redeemer"];
-  export type OrderRedeemer = OrderValidateOrder["redeemer"];
-  export type SellerRedeemer = SellerValidateSellerSpending["redeemer"];
-  export type TreasuryRedeemer = TreasuryValidateTreasurySpending["redeemer"];
-
-  export function toRedeemerFactory(redeemer: FactoryRedeemer): string {
-    return Data.to(redeemer, FactoryValidateFactory.redeemer);
-  }
-
-  export function toRedeemerMinting(redeemer: MintRedeemer): string {
-    return Data.to(redeemer, FactoryValidateFactoryMinting.redeemer);
-  }
-  export function toRedeemerTreasury(redeemer: TreasuryRedeemer): string {
-    return Data.to(redeemer, TreasuryValidateTreasurySpending.redeemer);
-  }
-
-  export function toRedeemerManager(redeemer: ManagerRedeemer): string {
-    return Data.to(redeemer, ManagerValidateManagerSpending.redeemer);
-  }
-
-  export function toRedeemerSellerSpend(redeemer: SellerRedeemer): string {
-    return Data.to(redeemer, SellerValidateSellerSpending.redeemer);
-  }
-
-  export function toRedeemerOrder(redeemer: OrderRedeemer): string {
-    return Data.to(redeemer, OrderValidateOrder.redeemer);
-  }
-
-  export type LbeV2Asset = { policyId: string; assetName: string };
-
-  export function toLbeV2Asset(asset: Asset): LbeV2Asset {
-    return {
-      policyId: asset.policyId,
-      assetName: asset.tokenName,
-    };
-  }
-  export function fromLbeV2Asset(asset: LbeV2Asset): Asset {
-    return {
-      policyId: asset.policyId,
-      tokenName: asset.assetName,
-    };
-  }
-}
-
 export namespace LbeV2Types {
   export enum ReceiverDatumType {
     NO_DATUM = 0,
@@ -119,10 +18,73 @@ export namespace LbeV2Types {
         hash: string;
       };
 
+  export namespace ReceiverDatum {
+    export function toPlutusData(data: ReceiverDatum): Constr<Data> {
+      switch (data.type) {
+        case ReceiverDatumType.NO_DATUM: {
+          return new Constr(0, []);
+        }
+        case ReceiverDatumType.DATUM_HASH: {
+          return new Constr(1, [data.hash]);
+        }
+        case ReceiverDatumType.INLINE_DATUM: {
+          return new Constr(2, [data.hash]);
+        }
+      }
+    }
+
+    export function fromPlutusData(data: Constr<Data>): ReceiverDatum {
+      switch (data.index) {
+        case 0: {
+          return { type: ReceiverDatumType.NO_DATUM };
+        }
+        case 1: {
+          return {
+            type: ReceiverDatumType.DATUM_HASH,
+            hash: data.fields[0] as string,
+          };
+        }
+        case 2: {
+          return {
+            type: ReceiverDatumType.INLINE_DATUM,
+            hash: data.fields[0] as string,
+          };
+        }
+        default: {
+          throw Error(
+            `Index of Receiver Datum must be 0, 1 or 2, actual: ${data.index}`
+          );
+        }
+      }
+    }
+  }
+
   export type PenaltyConfig = {
     penaltyStartTime: bigint;
     percent: bigint;
   };
+
+  export namespace PenaltyConfig {
+    export function toPlutusData(data: PenaltyConfig): Constr<Data> {
+      return new Constr(0, [data.penaltyStartTime, data.percent]);
+    }
+
+    export function fromPlutusData(data: Constr<Data>): PenaltyConfig {
+      switch (data.index) {
+        case 0: {
+          return {
+            penaltyStartTime: data.fields[0] as bigint,
+            percent: data.fields[1] as bigint,
+          };
+        }
+        default: {
+          throw Error(
+            `Index of Penalty Config must be 0, actual: ${data.index}`
+          );
+        }
+      }
+    }
+  }
 
   export type TreasuryDatum = {
     factoryPolicyId: string;
@@ -156,15 +118,188 @@ export namespace LbeV2Types {
     isManagerCollected: boolean;
   };
 
+  export namespace Options {
+    export function toPlutusData<T>(
+      data: T | undefined,
+      toPlutusDataFn: (data: T) => Data
+    ): Constr<Data> {
+      return data !== undefined
+        ? new Constr(0, [toPlutusDataFn(data)])
+        : new Constr(1, []);
+    }
+
+    export function fromPlutusData<T>(
+      data: Constr<Data>,
+      fromPlutusDataFn: (data: Constr<Data>) => T
+    ): T | undefined {
+      switch (data.index) {
+        case 0: {
+          return fromPlutusDataFn(data.fields[0] as Constr<Data>);
+        }
+        case 1: {
+          return undefined;
+        }
+        default: {
+          throw Error(`Index of Options must be 0 or 1, actual: ${data.index}`);
+        }
+      }
+    }
+  }
+
+  export namespace Dummy {
+    export function toPlutusData(x: Data): Data {
+      return x;
+    }
+    export function fromPlutusData<T>(x: Data): T {
+      return x as T;
+    }
+  }
+
+  export namespace Bool {
+    export function toPlutusData(data: boolean): Constr<Data> {
+      return data ? new Constr(1, []) : new Constr(0, []);
+    }
+
+    export function fromPlutusData(data: Constr<Data>): boolean {
+      switch (data.index) {
+        case 0: {
+          return false;
+        }
+        case 1: {
+          return true;
+        }
+        default: {
+          throw Error(`Index of Bool must be 0 or 1, actual: ${data.index}`);
+        }
+      }
+    }
+  }
+
   export namespace TreasuryDatum {
-    export function toPlutusSchema(
-      datum: TreasuryDatum
-    ): LbeV2PlutusSchema.TreasuryDatum {
+    export function toPlutusData(datum: TreasuryDatum): Constr<Data> {
+      const {
+        factoryPolicyId,
+        managerHash,
+        sellerHash,
+        orderHash,
+        baseAsset,
+
+        raiseAsset,
+        startTime,
+        endTime,
+        owner,
+        receiver,
+
+        receiverDatum,
+        poolAllocation,
+        minimumOrderRaise,
+        minimumRaise,
+        maximumRaise,
+
+        reserveBase,
+        penaltyConfig,
+        poolBaseFee,
+        revocable,
+        collectedFund,
+
+        reserveRaise,
+        totalPenalty,
+        totalLiquidity,
+        isCancelled,
+        isManagerCollected,
+      } = datum;
+      return new Constr(0, [
+        factoryPolicyId,
+        managerHash,
+        sellerHash,
+        orderHash,
+        Asset.toPlutusData(baseAsset),
+
+        Asset.toPlutusData(raiseAsset),
+        startTime,
+        endTime,
+        AddressPlutusData.toPlutusData(owner),
+        AddressPlutusData.toPlutusData(receiver),
+
+        ReceiverDatum.toPlutusData(receiverDatum),
+        poolAllocation,
+        Options.toPlutusData(minimumOrderRaise, Dummy.toPlutusData),
+        Options.toPlutusData(minimumRaise, Dummy.toPlutusData),
+        Options.toPlutusData(maximumRaise, Dummy.toPlutusData),
+
+        reserveBase,
+        Options.toPlutusData(penaltyConfig, PenaltyConfig.toPlutusData),
+        poolBaseFee,
+        Bool.toPlutusData(revocable),
+        collectedFund,
+
+        reserveRaise,
+        totalPenalty,
+        totalLiquidity,
+        Bool.toPlutusData(isCancelled),
+        Bool.toPlutusData(isManagerCollected),
+      ]);
+    }
+
+    export function fromPlutusData(
+      networkId: NetworkId,
+      data: Constr<Data>
+    ): TreasuryDatum {
+      if (data.index !== 0) {
+        throw new Error(
+          `Index of Treasury Datum must be 0, actual: ${data.index}`
+        );
+      }
+
+      const fields = data.fields;
       return {
-        ...datum,
-        baseAsset: LbeV2PlutusSchema.toLbeV2Asset(datum.baseAsset),
-        raiseAsset: LbeV2PlutusSchema.toLbeV2Asset(datum.raiseAsset),
-        owner: AddressPlutusData
+        factoryPolicyId: fields[0] as string,
+        managerHash: fields[1] as string,
+        sellerHash: fields[2] as string,
+        orderHash: fields[3] as string,
+        baseAsset: Asset.fromPlutusData(fields[4] as Constr<Data>),
+
+        raiseAsset: Asset.fromPlutusData(fields[5] as Constr<Data>),
+        startTime: fields[6] as bigint,
+        endTime: fields[7] as bigint,
+        owner: AddressPlutusData.fromPlutusData(
+          networkId,
+          fields[8] as Constr<Data>
+        ),
+        receiver: AddressPlutusData.fromPlutusData(
+          networkId,
+          fields[9] as Constr<Data>
+        ),
+
+        receiverDatum: ReceiverDatum.fromPlutusData(fields[10] as Constr<Data>),
+        poolAllocation: fields[11] as bigint,
+        minimumOrderRaise: Options.fromPlutusData<bigint>(
+          fields[12] as Constr<Data>,
+          Dummy.fromPlutusData
+        ),
+        minimumRaise: Options.fromPlutusData<bigint>(
+          fields[13] as Constr<Data>,
+          Dummy.fromPlutusData
+        ),
+        maximumRaise: Options.fromPlutusData<bigint>(
+          fields[14] as Constr<Data>,
+          Dummy.fromPlutusData
+        ),
+
+        reserveBase: fields[15] as bigint,
+        penaltyConfig: Options.fromPlutusData(
+          fields[16] as Constr<Data>,
+          PenaltyConfig.fromPlutusData
+        ),
+        poolBaseFee: fields[17] as bigint,
+        revocable: Bool.fromPlutusData(fields[18] as Constr<Data>),
+        collectedFund: fields[19] as bigint,
+
+        reserveRaise: fields[20] as bigint,
+        totalPenalty: fields[21] as bigint,
+        totalLiquidity: fields[22] as bigint,
+        isCancelled: Bool.fromPlutusData(fields[23] as Constr<Data>),
+        isManagerCollected: Bool.fromPlutusData(fields[24] as Constr<Data>),
       };
     }
   }
@@ -188,19 +323,23 @@ export namespace LbeV2Types {
 
   export namespace LbeV2Parameters {
     export function toLbeV2TreasuryDatum(
+      networkId: NetworkId,
       lbeV2Parameters: LbeV2Parameters
-    ): LbeV2Schema.TreasuryDatum {
-      const treasuryDatum: LbeV2Schema.TreasuryDatum = {
-        factoryPolicyId: warehouse.getFactoryHash(),
-        managerHash: warehouse.getManagerHash(),
-        sellerHash: warehouse.getSellerHash(),
-        orderHash: warehouse.getOrderHash(),
-        baseAsset: LbeV2Schema.toLbeV2Asset(lbeV2Parameters.baseAsset),
-        raiseAsset: LbeV2Schema.toLbeV2Asset(lbeV2Parameters.raiseAsset),
+    ): TreasuryDatum {
+      const config = LbeV2Constant.CONFIG[networkId];
+      const treasuryDatum: TreasuryDatum = {
+        factoryPolicyId: config.factoryHash,
+        managerHash: config.managerHash,
+        sellerHash: config.sellerHash,
+        orderHash: config.orderHash,
+        baseAsset: lbeV2Parameters.baseAsset,
+
+        raiseAsset: lbeV2Parameters.baseAsset,
         startTime: lbeV2Parameters.startTime,
         endTime: lbeV2Parameters.endTime,
         owner: lbeV2Parameters.owner,
         receiver: lbeV2Parameters.receiver,
+
         receiverDatum: {
           type: ReceiverDatumType.NO_DATUM,
         },
@@ -208,11 +347,13 @@ export namespace LbeV2Types {
         minimumOrderRaise: lbeV2Parameters.minimumOrderRaise,
         minimumRaise: lbeV2Parameters.minimumRaise,
         maximumRaise: lbeV2Parameters.maximumRaise,
+
         reserveBase: lbeV2Parameters.reserveBase,
         penaltyConfig: lbeV2Parameters.penaltyConfig,
         poolBaseFee: lbeV2Parameters.poolBaseFee,
         revocable: lbeV2Parameters.revocable,
         collectedFund: 0n,
+
         reserveRaise: 0n,
         totalPenalty: 0n,
         totalLiquidity: 0n,
@@ -220,6 +361,169 @@ export namespace LbeV2Types {
         isManagerCollected: false,
       };
       return treasuryDatum;
+    }
+  }
+
+  export type FactoryDatum = {
+    head: string;
+    tail: string;
+  };
+
+  export namespace FactoryDatum {
+    export function toPlutusData(data: FactoryDatum): Constr<Data> {
+      return new Constr(0, [data.head, data.tail]);
+    }
+
+    export function fromPlutusData(data: Constr<Data>): FactoryDatum {
+      switch (data.index) {
+        case 0: {
+          return {
+            head: data.fields[0] as string,
+            tail: data.fields[1] as string,
+          };
+        }
+        default: {
+          throw Error(`Index of FactoryDatum must be 0, actual: ${data.index}`);
+        }
+      }
+    }
+  }
+
+  export type ManagerDatum = {
+    factoryPolicyId: string;
+    baseAsset: Asset;
+    raiseAsset: Asset;
+    sellerCount: bigint;
+    reserveRaise: bigint;
+    totalPenalty: bigint;
+  };
+
+  export namespace ManagerDatum {
+    export function toPlutusData(data: ManagerDatum): Constr<Data> {
+      return new Constr(0, [
+        data.factoryPolicyId,
+        Asset.toPlutusData(data.baseAsset),
+        Asset.toPlutusData(data.raiseAsset),
+        data.sellerCount,
+        data.reserveRaise,
+        data.totalPenalty,
+      ]);
+    }
+
+    export function fromPlutusData(data: Constr<Data>): ManagerDatum {
+      switch (data.index) {
+        case 0: {
+          const fields = data.fields;
+          return {
+            factoryPolicyId: fields[0] as string,
+            baseAsset: Asset.fromPlutusData(fields[1] as Constr<Data>),
+            raiseAsset: Asset.fromPlutusData(fields[2] as Constr<Data>),
+            sellerCount: fields[3] as bigint,
+            reserveRaise: fields[4] as bigint,
+            totalPenalty: fields[5] as bigint,
+          };
+        }
+        default: {
+          throw Error(`Index of FactoryDatum must be 0, actual: ${data.index}`);
+        }
+      }
+    }
+  }
+
+  export type SellerDatum = {
+    factoryPolicyId: string;
+    owner: Address;
+    baseAsset: Asset;
+    raiseAsset: Asset;
+    amount: bigint;
+    penaltyAmount: bigint;
+  };
+
+  export namespace SellerDatum {
+    export function toPlutusData(data: SellerDatum): Constr<Data> {
+      return new Constr(0, [
+        data.factoryPolicyId,
+        AddressPlutusData.toPlutusData(data.owner),
+        Asset.toPlutusData(data.baseAsset),
+        Asset.toPlutusData(data.raiseAsset),
+        data.amount,
+        data.penaltyAmount,
+      ]);
+    }
+
+    export function fromPlutusData(
+      data: Constr<Data>,
+      networkId: NetworkId
+    ): SellerDatum {
+      switch (data.index) {
+        case 0: {
+          const fields = data.fields;
+          return {
+            factoryPolicyId: fields[0] as string,
+            owner: AddressPlutusData.fromPlutusData(
+              networkId,
+              fields[1] as Constr<Data>
+            ),
+            baseAsset: Asset.fromPlutusData(fields[2] as Constr<Data>),
+            raiseAsset: Asset.fromPlutusData(fields[3] as Constr<Data>),
+            amount: fields[4] as bigint,
+            penaltyAmount: fields[5] as bigint,
+          };
+        }
+        default: {
+          throw Error(`Index of SellerDatum must be 0, actual: ${data.index}`);
+        }
+      }
+    }
+  }
+
+  export type OrderDatum = {
+    factoryPolicyId: string;
+    baseAsset: Asset;
+    raiseAsset: Asset;
+    owner: Address;
+    amount: bigint;
+    isCollected: boolean;
+    penaltyAmount: bigint;
+  };
+
+  export namespace OrderDatum {
+    export function toPlutusData(data: OrderDatum): Constr<Data> {
+      return new Constr(0, [
+        data.factoryPolicyId,
+        Asset.toPlutusData(data.baseAsset),
+        Asset.toPlutusData(data.raiseAsset),
+        AddressPlutusData.toPlutusData(data.owner),
+        data.amount,
+        Bool.toPlutusData(data.isCollected),
+        data.penaltyAmount,
+      ]);
+    }
+
+    export function fromPlutusData(
+      data: Constr<Data>,
+      networkId: NetworkId
+    ): OrderDatum {
+      switch (data.index) {
+        case 0: {
+          const fields = data.fields;
+          return {
+            factoryPolicyId: fields[0] as string,
+            baseAsset: Asset.fromPlutusData(fields[1] as Constr<Data>),
+            raiseAsset: Asset.fromPlutusData(fields[2] as Constr<Data>),
+            owner: AddressPlutusData.fromPlutusData(
+              networkId,
+              fields[3] as Constr<Data>
+            ),
+            amount: fields[4] as bigint,
+            isCollected: Bool.fromPlutusData(fields[5] as Constr<Data>),
+            penaltyAmount: fields[6] as bigint,
+          };
+        }
+        default: {
+          throw Error(`Index of OrderDatum must be 0, actual: ${data.index}`);
+        }
+      }
     }
   }
 }
