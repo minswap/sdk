f6ec6403e805ddeada057ffe3b1e38d9655f86a4
Vardominator <vbarsegyan@gomaestro.org>
2024-12-15 15:38:15 +0000

    add maestro adapter tests

--- test/adapter.test.ts
+++ test/adapter.test.ts
@@ -1,8 +1,9 @@
 import { BlockFrostAPI } from "@blockfrost/blockfrost-js";
-
 import { ADA, Asset, NetworkId, StableswapConstant } from "../src";
-
 import { BlockfrostAdapter } from "../src/adapters/blockfrost";
+import { MaestroAdapter } from "../src/adapters/maestro";
+import { MaestroClient, Configuration } from "@maestro-org/typescript-sdk";
+import { Network } from "@minswap/lucid-cardano";
 
 function mustGetEnv(key: string): string {
   const val = process.env[key];
@@ -21,164 +22,186 @@ const MIN_ADA_POOL_V1_ID_TESTNET =
 const MIN_ADA_POOL_V1_ID_MAINNET =
   "6aa2153e1ae896a95539c9d62f76cedcdabdcdf144e564b8955f609d660cf6a2";
 
-let adapterTestnet: BlockfrostAdapter;
-let adapterMainnet: BlockfrostAdapter;
+let blockfrostAdapterTestnet: BlockfrostAdapter;
+let blockfrostAdapterMainnet: BlockfrostAdapter;
+let maestroAdapterTestnet: MaestroAdapter;
+let maestroAdapterMainnet: MaestroAdapter;
 
 beforeAll(() => {
-  adapterTestnet = new BlockfrostAdapter(
+  blockfrostAdapterTestnet = new BlockfrostAdapter(
     NetworkId.TESTNET,
     new BlockFrostAPI({
       projectId: mustGetEnv("BLOCKFROST_PROJECT_ID_TESTNET"),
       network: "preprod",
     })
   );
-  adapterMainnet = new BlockfrostAdapter(
+  blockfrostAdapterMainnet = new BlockfrostAdapter(
     NetworkId.MAINNET,
     new BlockFrostAPI({
       projectId: mustGetEnv("BLOCKFROST_PROJECT_ID_MAINNET"),
       network: "mainnet",
     })
   );
+  const cardanoNetworkPreprod: Network = "Preprod";
+  maestroAdapterTestnet = new MaestroAdapter(
+    NetworkId.TESTNET,
+    new MaestroClient(
+      new Configuration({
+        apiKey: "MAESTRO_API_KEY_TESTNET",
+        network: cardanoNetworkPreprod,
+      })
+    )
+  );
+  const cardanoNetworkMainnet: Network = "Mainnet";
+  maestroAdapterMainnet = new MaestroAdapter(
+    NetworkId.MAINNET,
+    new MaestroClient(
+      new Configuration({
+        apiKey: "MAESTRO_API_KEY_MAINNET",
+        network: cardanoNetworkMainnet,
+      })
+    )
+  );
 });
 
-test("getAssetDecimals", async () => {
-  expect(await adapterTestnet.getAssetDecimals("lovelace")).toBe(6);
-  expect(await adapterTestnet.getAssetDecimals(MIN_TESTNET)).toBe(0);
-  expect(await adapterMainnet.getAssetDecimals("lovelace")).toBe(6);
-  expect(await adapterMainnet.getAssetDecimals(MIN_MAINNET)).toBe(6);
-});
-
-async function testPoolPrice(adapter: BlockfrostAdapter): Promise<void> {
-  const pools = await adapter.getV1Pools({
-    page: 1,
+async function runTests(adapterTestnet: any, adapterMainnet: any) {
+  test("getAssetDecimals", async () => {
+    expect(await adapterTestnet.getAssetDecimals("lovelace")).toBe(6);
+    expect(await adapterTestnet.getAssetDecimals(MIN_TESTNET)).toBe(0);
+    expect(await adapterMainnet.getAssetDecimals("lovelace")).toBe(6);
+    expect(await adapterMainnet.getAssetDecimals(MIN_MAINNET)).toBe(6);
   });
-  expect(pools.length).toBeGreaterThan(0);
-  // check random 5 pools
-  for (let i = 0; i < 5; i++) {
-    const idx = Math.floor(Math.random() * pools.length);
-    const pool = pools[idx];
-    const [priceAB, priceBA] = await adapter.getV1PoolPrice({ pool });
-    // product of 2 prices must be approximately equal to 1
-    // abs(priceAB * priceBA - 1) <= epsilon
-    expect(priceAB.mul(priceBA).sub(1).abs().toNumber()).toBeLessThanOrEqual(
-      1e-6
-    );
+
+  async function testPoolPrice(adapter: any): Promise<void> {
+    const pools = await adapter.getV1Pools({ page: 1 });
+    expect(pools.length).toBeGreaterThan(0);
+    for (let i = 0; i < 5; i++) {
+      const idx = Math.floor(Math.random() * pools.length);
+      const pool = pools[idx];
+      const [priceAB, priceBA] = await adapter.getV1PoolPrice({ pool });
+      expect(priceAB.mul(priceBA).sub(1).abs().toNumber()).toBeLessThanOrEqual(
+        1e-6
+      );
+    }
   }
+
+  test("getPoolPrice", async () => {
+    await testPoolPrice(adapterTestnet);
+    await testPoolPrice(adapterMainnet);
+  }, 10000);
+
+  test("getV1PoolById", async () => {
+    const adaMINTestnet = await adapterTestnet.getV1PoolById({
+      id: MIN_ADA_POOL_V1_ID_TESTNET,
+    });
+    expect(adaMINTestnet).not.toBeNull();
+    expect(adaMINTestnet?.assetA).toEqual("lovelace");
+    expect(adaMINTestnet?.assetB).toEqual(MIN_TESTNET);
+
+    const adaMINMainnet = await adapterMainnet.getV1PoolById({
+      id: MIN_ADA_POOL_V1_ID_MAINNET,
+    });
+    expect(adaMINMainnet).not.toBeNull();
+    expect(adaMINMainnet?.assetA).toEqual("lovelace");
+    expect(adaMINMainnet?.assetB).toEqual(MIN_MAINNET);
+  });
+
+  async function testPriceHistory(adapter: any, id: string): Promise<void> {
+    const history = await adapter.getV1PoolHistory({ id });
+    for (let i = 0; i < Math.min(5, history.length); i++) {
+      const pool = await adapter.getV1PoolInTx({ txHash: history[i].txHash });
+      expect(pool?.txIn.txHash).toEqual(history[i].txHash);
+    }
+  }
+
+  test("get prices of last 5 states of MIN/ADA pool", async () => {
+    await testPriceHistory(adapterTestnet, MIN_ADA_POOL_V1_ID_TESTNET);
+    await testPriceHistory(adapterMainnet, MIN_ADA_POOL_V1_ID_MAINNET);
+  });
+
+  test("getV2PoolByPair", async () => {
+    const pool = await adapterTestnet.getV2PoolByPair(ADA, {
+      policyId: "e16c2dc8ae937e8d3790c7fd7168d7b994621ba14ca11415f39fed72",
+      tokenName: "4d494e",
+    });
+    expect(pool).not.toBeNull();
+    expect(pool?.assetA).toEqual("lovelace");
+    expect(pool?.assetB).toEqual(MIN_TESTNET);
+  });
+
+  test("getAllV2Pools", async () => {
+    const { pools } = await adapterTestnet.getAllV2Pools();
+    expect(pools.length > 0);
+  });
+
+  test("getV2Pools", async () => {
+    const { pools } = await adapterTestnet.getV2Pools({ page: 1 });
+    expect(pools.length > 0);
+  });
+
+  test("getAllStablePools", async () => {
+    const numberOfStablePoolsTestnet =
+      StableswapConstant.CONFIG[NetworkId.TESTNET].length;
+    const numberOfStablePoolsMainnet =
+      StableswapConstant.CONFIG[NetworkId.MAINNET].length;
+    const { pools: testnetPools } = await adapterTestnet.getAllStablePools();
+    expect(testnetPools.length === numberOfStablePoolsTestnet);
+
+    const { pools: mainnetPools } = await adapterMainnet.getAllStablePools();
+    expect(mainnetPools.length === numberOfStablePoolsMainnet);
+  });
+
+  test("getStablePoolByLPAsset", async () => {
+    const testnetCfgs = StableswapConstant.CONFIG[NetworkId.TESTNET];
+    const mainnetCfgs = StableswapConstant.CONFIG[NetworkId.MAINNET];
+
+    for (const cfg of testnetCfgs) {
+      const pool = await adapterTestnet.getStablePoolByLpAsset(
+        Asset.fromString(cfg.lpAsset)
+      );
+      expect(pool).not.toBeNull();
+      expect(pool?.nft).toEqual(cfg.nftAsset);
+      expect(pool?.assets).toEqual(cfg.assets);
+    }
+
+    for (const cfg of mainnetCfgs) {
+      const pool = await adapterMainnet.getStablePoolByLpAsset(
+        Asset.fromString(cfg.lpAsset)
+      );
+      expect(pool).not.toBeNull();
+      expect(pool?.nft).toEqual(cfg.nftAsset);
+      expect(pool?.assets).toEqual(cfg.assets);
+    }
+  });
+
+  test("getStablePoolByNFT", async () => {
+    const testnetCfgs = StableswapConstant.CONFIG[NetworkId.TESTNET];
+    const mainnetCfgs = StableswapConstant.CONFIG[NetworkId.MAINNET];
+
+    for (const cfg of testnetCfgs) {
+      const pool = await adapterTestnet.getStablePoolByNFT(
+        Asset.fromString(cfg.nftAsset)
+      );
+      expect(pool).not.toBeNull();
+      expect(pool?.nft).toEqual(cfg.nftAsset);
+      expect(pool?.assets).toEqual(cfg.assets);
+    }
+
+    for (const cfg of mainnetCfgs) {
+      const pool = await adapterMainnet.getStablePoolByNFT(
+        Asset.fromString(cfg.nftAsset)
+      );
+      expect(pool).not.toBeNull();
+      expect(pool?.nft).toEqual(cfg.nftAsset);
+      expect(pool?.assets).toEqual(cfg.assets);
+    }
+  });
 }
 
-test("getPoolPrice", async () => {
-  await testPoolPrice(adapterTestnet);
-  await testPoolPrice(adapterMainnet);
-}, 10000);
-
-test("getV1PoolById", async () => {
-  const adaMINTestnet = await adapterTestnet.getV1PoolById({
-    id: MIN_ADA_POOL_V1_ID_TESTNET,
-  });
-  expect(adaMINTestnet).not.toBeNull();
-  expect(adaMINTestnet?.assetA).toEqual("lovelace");
-  expect(adaMINTestnet?.assetB).toEqual(MIN_TESTNET);
-
-  const adaMINMainnet = await adapterMainnet.getV1PoolById({
-    id: MIN_ADA_POOL_V1_ID_MAINNET,
-  });
-  expect(adaMINMainnet).not.toBeNull();
-  expect(adaMINMainnet?.assetA).toEqual("lovelace");
-  expect(adaMINMainnet?.assetB).toEqual(MIN_MAINNET);
+describe("BlockfrostAdapter Tests", () => {
+  runTests(blockfrostAdapterTestnet, blockfrostAdapterMainnet);
 });
 
-async function testPriceHistory(
-  adapter: BlockfrostAdapter,
-  id: string
-): Promise<void> {
-  const history = await adapter.getV1PoolHistory({ id: id });
-  for (let i = 0; i < Math.min(5, history.length); i++) {
-    const pool = await adapter.getV1PoolInTx({ txHash: history[i].txHash });
-    expect(pool?.txIn.txHash).toEqual(history[i].txHash);
-  }
-}
-
-test("get prices of last 5 states of MIN/ADA pool", async () => {
-  await testPriceHistory(adapterTestnet, MIN_ADA_POOL_V1_ID_TESTNET);
-  await testPriceHistory(adapterMainnet, MIN_ADA_POOL_V1_ID_MAINNET);
-});
-
-test("getV2PoolByPair", async () => {
-  const pool = await adapterTestnet.getV2PoolByPair(ADA, {
-    policyId: "e16c2dc8ae937e8d3790c7fd7168d7b994621ba14ca11415f39fed72",
-    tokenName: "4d494e",
-  });
-  expect(pool).not.toBeNull();
-  expect(pool?.assetA).toEqual("lovelace");
-  expect(pool?.assetB).toEqual(MIN_TESTNET);
-});
-
-test("getAllV2Pools", async () => {
-  const { pools } = await adapterTestnet.getAllV2Pools();
-  expect(pools.length > 0);
-});
-
-test("getV2Pools", async () => {
-  const { pools } = await adapterTestnet.getV2Pools({
-    page: 1,
-  });
-  expect(pools.length > 0);
-});
-
-test("getAllStablePools", async () => {
-  const numberOfStablePoolsTestnet =
-    StableswapConstant.CONFIG[NetworkId.TESTNET].length;
-  const numberOfStablePoolsMainnet =
-    StableswapConstant.CONFIG[NetworkId.MAINNET].length;
-  const { pools: testnetPools } = await adapterTestnet.getAllStablePools();
-  expect(testnetPools.length === numberOfStablePoolsTestnet);
-
-  const { pools: mainnetPools } = await adapterMainnet.getAllStablePools();
-  expect(mainnetPools.length === numberOfStablePoolsMainnet);
-});
-
-test("getStablePoolByLPAsset", async () => {
-  const testnetCfgs = StableswapConstant.CONFIG[NetworkId.TESTNET];
-  const mainnetCfgs = StableswapConstant.CONFIG[NetworkId.MAINNET];
-
-  for (const cfg of testnetCfgs) {
-    const pool = await adapterTestnet.getStablePoolByLpAsset(
-      Asset.fromString(cfg.lpAsset)
-    );
-    expect(pool).not.toBeNull();
-    expect(pool?.nft).toEqual(cfg.nftAsset);
-    expect(pool?.assets).toEqual(cfg.assets);
-  }
-
-  for (const cfg of mainnetCfgs) {
-    const pool = await adapterMainnet.getStablePoolByLpAsset(
-      Asset.fromString(cfg.lpAsset)
-    );
-    expect(pool).not.toBeNull();
-    expect(pool?.nft).toEqual(cfg.nftAsset);
-    expect(pool?.assets).toEqual(cfg.assets);
-  }
-});
-
-test("getStablePoolByNFT", async () => {
-  const testnetCfgs = StableswapConstant.CONFIG[NetworkId.TESTNET];
-  const mainnetCfgs = StableswapConstant.CONFIG[NetworkId.MAINNET];
-
-  for (const cfg of testnetCfgs) {
-    const pool = await adapterTestnet.getStablePoolByNFT(
-      Asset.fromString(cfg.nftAsset)
-    );
-    expect(pool).not.toBeNull();
-    expect(pool?.nft).toEqual(cfg.nftAsset);
-    expect(pool?.assets).toEqual(cfg.assets);
-  }
-
-  for (const cfg of mainnetCfgs) {
-    const pool = await adapterMainnet.getStablePoolByNFT(
-      Asset.fromString(cfg.nftAsset)
-    );
-    expect(pool).not.toBeNull();
-    expect(pool?.nft).toEqual(cfg.nftAsset);
-    expect(pool?.assets).toEqual(cfg.assets);
-  }
+describe("MaestroAdapter Tests", () => {
+  runTests(maestroAdapterTestnet, maestroAdapterMainnet);
 });
