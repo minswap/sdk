89050e38511fadca011027f02033922b8ac53af2
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-05 16:08:03 +0700

    fix getScriptHashFromAddress

--- src/utils/address-utils.internal.ts
+++ src/utils/address-utils.internal.ts
@@ -1,10 +1,14 @@
-import { Addresses } from "@spacebudz/lucid";
+import { Addresses, Utils } from "@spacebudz/lucid";
 
 export function getScriptHashFromAddress(addr: string): string | null { // TODO
   try {
     const addrDetail = Addresses.inspect(addr);
-    const scriptHash =  addrDetail.delegation?.hash;
-    return scriptHash ? scriptHash : null;
+    const scriptHash =  addrDetail.payment?.hash;
+    if (!scriptHash) {
+      return null
+    }
+
+    return Utils.encodeBech32('script', scriptHash);
   } catch {
     return null
   }
