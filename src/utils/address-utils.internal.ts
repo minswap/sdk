44c03dc883c045a3e51fd0350095c960bd22e9c9
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-05 09:15:19 +0700

    fix getScriptHashFromAddress

--- src/utils/address-utils.internal.ts
+++ src/utils/address-utils.internal.ts
@@ -1,19 +1,10 @@
-import { Utils } from "@spacebudz/lucid/mod";
+import { Addresses } from "@spacebudz/lucid/mod";
 
 export function getScriptHashFromAddress(addr: string): string | null { // TODO
   try {
-    const cslAddr = C.Address.from_bech32(addr);
-    const specificAddr =
-      C.BaseAddress.from_address(cslAddr) ||
-      C.EnterpriseAddress.from_address(cslAddr) ||
-      C.PointerAddress.from_address(cslAddr) ||
-      C.RewardAddress.from_address(cslAddr);
-    if (!specificAddr) {
-      return null;
-    }
-    return (
-      specificAddr.payment_cred().to_scripthash()?.to_bech32("script") ?? null
-    );
+    const addrDetail = Addresses.inspect(addr);
+    const scriptHash =  addrDetail.delegation?.hash;
+    return scriptHash ? scriptHash : null;
   } catch {
     return null
   }
